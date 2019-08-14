27f00e8ae394fa512a9dc5c70b2b878c33bcb49b
PurpleCapedCrusader <alexcotton@mac.com>
2019-08-14 07:37:06 -0700

    git keychain update test

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class Rocket : MonoBehaviour
 {
 
-    [SerializeField] float mainThrust = 102f;
+    [SerializeField] float mainThrust = 103f;
     [SerializeField] float rotateThrust = 100f;
     Rigidbody rigidBody;
     [SerializeField] AudioClip mainEngine;
